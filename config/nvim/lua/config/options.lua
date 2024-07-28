adef51df713aec37552152048b7d92a004140cc2
Joshua Peek <josh@joshpeek.com>
2024-07-27 18:38:35 -0700

    Move option stuff into options.lua

---
+++ config/nvim/lua/config/options.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Write without backups
+vim.g.writebackup = false
+
+-- I don't wear extra large tabs
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Neovide on macOS
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_position_animation_length = 0
+  vim.g.neovide_scroll_animation_length = 0
+end
