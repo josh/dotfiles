4c80d6ac129288569bb79875a3181398c6cfe2af
Joshua Peek <josh@joshpeek.com>
2024-07-27 18:52:54 -0700

    Fix clipboard

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -13,4 +13,5 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_position_animation_length = 0
   vim.g.neovide_scroll_animation_length = 0
+  vim.opt.clipboard = "unnamedplus"
 end
