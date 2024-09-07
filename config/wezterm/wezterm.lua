5ac90bf1bde70bf888c356562bbbc8bda11f7724
Joshua Peek <josh@joshpeek.com>
2024-09-07 11:49:30 -0700

    Shake up Wez theme

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,18 +4,18 @@ local act = wezterm.action
 
 config.set_environment_variables = {
 	PATH = "/opt/homebrew/bin:" .. os.getenv("PATH"),
-	THEME = "tokyonight_moon",
+	THEME = "catppuccin_macchiato",
 }
 
 -- local appearance = require("appearance")
-config.color_scheme = "Tokyo Night Moon"
+config.color_scheme = "Catppuccin Macchiato"
 
-config.font = wezterm.font({ family = "Berkeley Mono" })
+config.font = wezterm.font({ family = "MesloLGM Nerd Font Mono" })
 config.font_size = 18
 
 config.window_decorations = "RESIZE"
 config.window_frame = {
-	font = wezterm.font({ family = "Berkeley Mono", weight = "Bold" }),
+	font = wezterm.font({ family = "MesloLGM Nerd Font Mono", weight = "Bold" }),
 	font_size = 16,
 }
 
