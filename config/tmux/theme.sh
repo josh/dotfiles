0d82a73eedf93221dce0144bf8cd880df429fafd
Joshua Peek <josh@joshpeek.com>
2024-10-07 23:04:12 -0700

    shfmt

--- config/tmux/theme.sh
+++ config/tmux/theme.sh
@@ -9,8 +9,8 @@ if [ -d /opt/homebrew/opt/tmux-tokyo-night ] && [[ $THEME == tokyonight* ]]; the
 
 elif [ -d /opt/homebrew/opt/tmux-catppuccin ] && [[ $THEME == catppuccin* ]]; then
 	catppuccin_flavor="${THEME#*_}"
-    tmux set-option -g '@catppuccin_flavor' "$catppuccin_flavor"
+	tmux set-option -g '@catppuccin_flavor' "$catppuccin_flavor"
 
 	# shellcheck source=/dev/null
-    source /opt/homebrew/opt/tmux-catppuccin/share/tmux-catppuccin/catppuccin.tmux
+	source /opt/homebrew/opt/tmux-catppuccin/share/tmux-catppuccin/catppuccin.tmux
 fi
